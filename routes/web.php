ecf28a677a45c6e0d8655d9aa989cd25b16bd8be
sanzida5 <tithi2007061@stud.kuet.ac.bd>
2024-12-05 10:25:52 +0600

    route update

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,27 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RegistrationController;
-
-Route::get('/signup', function () {
-    return view('signup');
-});
-Route::get('/login', function () {
-    return view('login');
-});
-
-// routes/web.php
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ProductController::class, 'index'])->name('index');
-Route::get('/products/create', [ProductController::class, 'create']);
-Route::post('/products', [ProductController::class, 'store']);
-Route::get('/products/{id}/edit', [ProductController::class, 'edit']);
-Route::put('/products/{id}', [ProductController::class, 'update']);
-Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-Route::get('/search', [ProductController::class, 'search']);
-
-
+// User Registration Routes
 Route::get('/register', [RegistrationController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegistrationController::class, 'register'])->name('register.user');
 
-
-// // Route to show registration form
-// Route::get('register', [AuthController::class, 'showRegistrationForm'])->name('register');
-
-// // Route to handle registration form submission
-// Route::post('register', [AuthController::class, 'register'])->name('register.submit');
-
-
-use App\Http\Controllers\AuthController;
-
+// User Login Routes
 Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('login', [AuthController::class, 'login'])->name('login.submit');
+
+// Product Routes
+Route::get('/', [ProductController::class, 'index'])->name('index');
+
+// Protected Routes: Only accessible by authenticated users
+Route::middleware('auth')->group(function () {
+    Route::get('/products/create', [ProductController::class, 'create']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::get('/products/{id}/edit', [ProductController::class, 'edit']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::get('/search', [ProductController::class, 'search']);
+    Route::get('/order/product/{id}', [ProductController::class, 'orderProduct'])->name('order.product');
+});
